a44620c0d00affbd3e226bc170f46efd3ee373a5
yanc <volcas@gmail.com>
2019-01-28 15:22:36 +0800

    change upload method

--- source/esp/extra_script.py
+++
@@ -1,9 +0,0 @@
-Import("env")
-
-# please keep $SOURCE variable, it will be replaced with a path to firmware
-
-# Generic
-env.Replace(
-    UPLOADER="python esptool.py "
-    UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCE"
-)
