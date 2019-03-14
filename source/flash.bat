95af862cfb8d54e905193950a8be667c86881803
yanc <volcas@gmail.com>
2019-03-14 18:24:28 +0800

    add erase command

--- source/flash.bat
+++ source/flash.bat
@@ -1,3 +1,6 @@
 pio run -t upload -d esp-programmer
-pio run -t upload -d esp
+ping -n 3 127.0.0.1
+pio run -e clean -t erase -d esp
+ping -n 10 127.0.0.1
+pio run -e whid  -t upload -d esp
 pio run -t upload -d 32u4
--- source/flash.sh
+++ source/flash.sh
@@ -1,3 +1,6 @@
 pio run -t upload -d esp-programmer
-pio run -t upload -d esp
+sleep 3
+pio run -e clean -t erase -d esp
+sleep 10
+pio run -e whid  -t upload -d esp
 pio run -t upload -d 32u4
